f09d10edb6b538c32f8ca3e670584479080847f0
Lucas Paula <luolcami@gmail.com>
2022-04-24 22:11:37 +0100

    final commit?

--- images_to_pdf/cli.py
+++ images_to_pdf/cli.py
@@ -1,14 +1,50 @@
 """Console script for images_to_pdf."""
 import sys
 import click
+from typing import List
+
+from PIL import Image, UnidentifiedImageError
+
+from images_to_pdf.images_to_pdf import execute
 
 
-@click.command()
-def main(args=None):
-    """Console script for images_to_pdf."""
-    click.echo("Replace this message by putting your code into "
-               "images_to_pdf.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+def sanitize_output_name(ctx, param, value: str):
+    if not value.endswith(".pdf"):
+        return f"{value}.pdf"
+    return value
+
+
+def verify_images(ctx, param, value: List[str]):
+    for image in value:
+        try:
+            Image.open(image).verify()
+        except UnidentifiedImageError:
+            raise click.BadParameter(
+                message=f"The file {repr(image)} is corrupted/not a valid image file 😞, check your files and try again 😉."
+            )
+    return value
+
+
+@click.command(context_settings={"ignore_unknown_options": True})
+@click.argument(
+    "image_files",
+    type=click.Path(exists=True),
+    nargs=-1,
+    required=True,
+    callback=verify_images,
+)
+@click.option(
+    "-n",
+    "--name",
+    show_default=True,
+    required=True,
+    help='Output name e.g. "out.pdf"',
+    prompt="Output name",
+    callback=sanitize_output_name,
+)
+def main(name, image_files):
+    """Transforms a list of images into a single pdf"""
+    execute(name, image_files)
     return 0
 
 
