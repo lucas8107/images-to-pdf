89ef4f3ce2210447c76bb7c9b49c1fe9e3e25e88
Lucas Paula <luolcami@gmail.com>
2022-04-24 22:49:46 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lucas8107/images-to-pdf",
-    version="version='version='0.1.0''",
+    version="version='version='0.1.1''",
     zip_safe=False,
 )
